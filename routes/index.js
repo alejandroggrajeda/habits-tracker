2dfd786d88d40e5d6d66236920ee97efbce14042
alejandroggrajeda <alejandroggrajeda@gmail.com>
2025-03-03 19:42:30 -0600

    manejo de errores de enpoints  mejorado

--- routes/index.js
+++ routes/index.js
@@ -19,33 +19,51 @@ router.get("/habits", async (req, res) => {
 router.post("/habits", async (req, res) => {
   try {
     const { title, description } = req.body;
-    const habit = new Habit({title, description});
+
+    if (!title || !description)
+      return res
+        .status(400)
+        .json({ error: "title and description are required." });
+
+    const habit = new Habit({ title, description });
     await habit.save();
+
     res.json(habit);
   } catch (error) {
-    res.status(400).json({ error: "error del servidor" });
+    res.status(500).json({ error: "error del servidor" });
   }
 });
 
-router.put('/habits/:id', async(req, res) => {
-  try{
-    const {title, description} = req.body;
-    const newHabit = {title, description};
-    const habit = await Habit.findByIdAndUpdate
-    (req.params.id, {title, description}, {new: true});
-    res.json({message: 'el Hábito fue actualizado exitósamente.', habit});
-  }catch(error){  
-    res.status(500).json({error: 'Habit not found.'})
-  }
-}
-);
+router.put("/habits/:id", async (req, res) => {
+  try {
+    const { title, description } = req.body;
 
-router.delete('/habits/:id', async(req, res) => {
-  try{
-    await Habit.findByIdAndDelete(req.params.id);
-    res.json({message: 'Habit deleted.'})
-  }catch(error){
-    res.status(500).json({error: 'Habit not found.'})
+    if (!title || !description)
+      return res
+        .status(400)
+        .json({ error: "title and description are required." });
+
+    const habit = await Habit.findByIdAndUpdate(
+      req.params.id,
+      { title, description },
+      { new: true, runValidators: true }
+    );
+
+    if (!habit) return res.status(404).json({ error: "Habit not found." });
+
+    res.json({ message: "el Hábito fue actualizado exitósamente.", habit });
+  } catch (error) {
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+router.delete("/habits/:id", async (req, res) => {
+  try {
+    const habit = await Habit.findByIdAndDelete(req.params.id);
+    if (!habit) res.status(404).json({ error: "Habit not found." });
+    res.json({ message: "Habit deleted." });
+  } catch (error) {
+    res.status(500).json({ error: "Server error." });
   }
 });
 
