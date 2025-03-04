f79c531af77d2aa6dc14c6773c5f41789b98e306
alejandroggrajeda <alejandroggrajeda@gmail.com>
2025-03-03 20:30:56 -0600

    arreglando conexion a la base de datos.

--- config/database.js
+++ config/database.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Database connected"))
-  .catch((err) => console.log(err));
+  .catch((error) => {
+    console.error("Database connection failed:", error.message);
+    process.exit(1);
+  });
 
 module.exports = mongoose;
-
