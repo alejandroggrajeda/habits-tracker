90100561c9de121afcca920e05f32de982c5de4b
alejandroggrajeda <alejandroggrajeda@gmail.com>
2025-03-13 23:10:41 -0600

    correcion de la configuracion de la base de datos

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, { dbname:"Galileo", useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Database connected"))
   .catch((error) => {
     console.error("Database connection failed:", error.message);
