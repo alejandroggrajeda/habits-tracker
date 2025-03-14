ad60bcdace8b75e5666bedf22cd05787ae328487
alejandroggrajeda <alejandroggrajeda@gmail.com>
2025-03-14 00:16:58 -0600

    cambiando colores a darkmode

--- frontend/app/habits.tsx
+++ frontend/app/habits.tsx
@@ -12,16 +12,16 @@ type HabitsProps = {
 
 export default function Habits({ habits }: HabitsProps) {
   return (
-    <div className="w-full max-w-md p-4 bg-white rounded-lg shadow-md mt-8">
-      <h1 className="text-2xl font-bold mb-4 text-black text-center">Habits Tracker</h1>
+    <div className="w-full max-w-md p-4 bg-slate-700 rounded-lg shadow-md mt-8">
+      <h1 className="text-2xl font-bold mb-4 text-gray-200 text-center">Habits Tracker</h1>
 
       <ul className="space-y-4">
         {habits.map((habit: Habit) => (
           <li className="flex items-center justify-between" key={habit._id}>
-            <span className="text-black">{habit.title}</span>
+            <span className="text-gray-200">{habit.title}</span>
             <div className="flex items-center space-x-2">
             <progress className="w-24" value={70} max={100}></progress>
-            <button className="px-2 py-1 text-sm text-white bg-blue-500 rounded" >Done</button>
+            <button className="px-2 py-1 text-sm text-gray-200 bg-blue-500 rounded" >Done</button>
             </div>
           </li>
         ))}
